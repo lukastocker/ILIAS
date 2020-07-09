b04350492fa60d833bf6f840019d0566222e98ba
lukastocker <luka.stocker@concepts-and-training.de>
2020-07-09 15:16:02 +0200

    Use DateTimeInterface for transformation.

--- src/Refinery/KindlyTo/Transformation/DateTimeTransformation.php
+++ src/Refinery/KindlyTo/Transformation/DateTimeTransformation.php
@@ -20,18 +20,12 @@ use ILIAS\Refinery\Transformation;
  */
 class DateTimeTransformation implements Transformation
 {
-    const Dt_Atom = 'Y-m-d\TH:i:sP';
-    const Reg_Atom = '/^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})\+([0-9]{2}):([0-9]{2})$/';
-    const Dt_Cookie = 'l, d-M-Y H:i:s T';
-    const Reg_Cookie = '/^([A-Za-z]+),\ ([0-9]{2})-([A-Z][a-z]+)-([0-9]{4})\ ([0-9]{2}):([0-9]{2}):([0-9]{2})\ ([A-Za-z]+)\+([0-9]{4})$/';
-    const Dt_ISO8601 = 'Y-m-d\TH:i:sO';
-    const Reg_ISO8601 = '/^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})\+([0-9]{4})$/';
-    const Dt_RFC822 = 'D, d M y H:i:s O';
-    const Reg_RFC822 = '/^([A-Za-z]+),\ ([0-9]{2})\ ([A-Z][a-z]+)\ ([0-9]{2})\ ([0-9]{2}):([0-9]{2}):([0-9]{2})\ \+([0-9]{4})$/';
-    const Dt_RFC7231 = 'D, d M Y H:i:s \G\M\T';
-    const Reg_RFC7231 = '/^([A-Za-z]+),\ ([0-9]{2})\ ([A-Za-z]+)\ ([0-9]{4})\ ([0-9]{2}):([0-9]{2}):([0-9]{2})\ ([A-Za-z]+)$/';
-    const Dt_RFC3339_ext = 'Y-m-d\TH:i:s.vP';
-    const Reg_RFC3339_ext = '/^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})\.([0-9]{3})\+([0-9]{2}):([0-9]{2})$/';
+    const Reg_Atom = '/^([0-9]{4})([-])([0-9]{2})([-])([0-9]{2})([T])([0-9]{2})([:])([0-9]{2})([:])([0-9]{2})([\)([+])([0-9]{2})([:])([0-9]{2})$/';
+    const Reg_Cookie = '/^([A-Za-z]+)([,])([\])([ ])([0-9]{2})([-])([A-Z][a-z]+)([-])([0-9]{4})([\])([ ])([0-9]{2})([:])([0-9]{2})([:])([0-9]{2})([\])([ ])([A-Za-z]+)([\])([+])([0-9]{4})$/';
+    const Reg_ISO8601 = '/^([0-9]{4})([-])([0-9]{2})([-])([0-9]{2})([T])([0-9]{2})([:])([0-9]{2})([:])([0-9]{2})([+])([0-9]{4})$/';
+    const Reg_RFC822 = '/^([A-Za-z]+)([,])([\])([ ])([0-9]{2})([\])([ ])([A-Z][a-z]+)([\])([ ])([0-9]{2})([\])([ ])([0-9]{2}):([0-9]{2})([:])([0-9]{2})([\])([ ])([\])([+])([0-9]{4})$/';
+    const Reg_RFC7231 = '/^([A-Za-z]+)([,])([\])([ ])([0-9]{2})([\])([ ])([A-Za-z]+)([\])([ ])([0-9]{4})([\])([ ])([0-9]{2})([:])([0-9]{2})([:])([0-9]{2})([\])([ ])([A-Za-z]+)$/';
+    const Reg_RFC3339_ext = '/^([0-9]{4})([-])([0-9]{2})([-])([0-9]{2})([T])([0-9]{2})([:])([0-9]{2})([:])([0-9]{2})([\])([.])([0-9]{3})([\])([+])([0-9]{2})([:])([0-9]{2})$/';
 
     use DeriveApplyToFromTransform;
 
@@ -44,39 +38,33 @@ class DateTimeTransformation implements Transformation
         {
             if(preg_match(self::Reg_Atom, $from, $RegMatch))
             {
-                $from = strval($from);
-                $DateImmutable = new \DateTimeImmutable($from);
-                return $DateImmutable->format(self::Dt_Atom);
+                $DateImmutable = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::ATOM, $from);
+                return $DateImmutable->format(\DateTimeImmutable::ATOM);
             }
             elseif(preg_match(self::Reg_Cookie, $from, $RegMatch))
             {
-                $from = strval($from);
-                $DateImmutable = new \DateTimeImmutable($from);
-                return $DateImmutable->format(self::Dt_Cookie);
+                $DateImmutable = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::COOKIE, $from);
+                return $DateImmutable->format(\DateTimeImmutable::COOKIE);
             }
             elseif(preg_match(self::Reg_ISO8601,$from,$RegMatch))
             {
-                $from = strval($from);
-                $DateImmutable = new \DateTimeImmutable($from);
-                return $DateImmutable->format(self::Dt_ISO8601);
+                $DateImmutable = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::ISO8601, $from);
+                return $DateImmutable->format(\DateTimeImmutable::ISO8601);
             }
             elseif(preg_match(self::Reg_RFC822,$from,$RegMatch))
             {
-                $from = strval($from);
-                $DateImmutable = new \DateTimeImmutable($from);
-                return $DateImmutable->format(self::Dt_RFC822);
+                $DateImmutable = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::RFC822, $from);
+                return $DateImmutable->format(\DateTimeImmutable::RFC822);
             }
             elseif(preg_match(self::Reg_RFC7231,$from,$RegMatch))
             {
-                $from = strval($from);
-                $DateImmutable = new \DateTimeImmutable($from);
-                return $DateImmutable->format(self::Dt_RFC7231);
+                $DateImmutable = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::RFC7231, $from);
+                return $DateImmutable->format(\DateTimeImmutable::RFC7231);
             }
             elseif(preg_match(self::Reg_RFC3339_ext,$from,$RegMatch))
             {
-                $from = strval($from);
-                $DateImmutable = new \DateTimeImmutable($from);
-                return $DateImmutable->format(self::Dt_RFC3339_ext);
+                $DateImmutable = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::RFC3339_EXTENDED, $from);
+                return $DateImmutable->format(\DateTimeImmutable::RFC3339_EXTENDED);
             }
         }
         elseif(true === is_int($from) || true === is_float($from))
@@ -87,7 +75,6 @@ class DateTimeTransformation implements Transformation
         {
             throw new \InvalidArgumentException("$from can not be transformed into DateTimeImmutable or Unix timestamp.", 1);
         }
-
     }
 
     /**
--- tests/Refinery/KindlyTo/Transformation/DateTimeTransformationTest.php
+++ tests/Refinery/KindlyTo/Transformation/DateTimeTransformationTest.php
@@ -19,8 +19,12 @@ use PHPUnit\Framework\TestCase;
 
 class DateTimeTransformationTest extends TestCase
 {
-    const Date_Origin = '2020-07-06T12:23:05+0000';
-    const ISO8601 = 'Y-m-d\TH:i:sO';
+    const Date_ISO = '2020-07-06T12:23:05+0000';
+    const Date_Atom ='2020-07-06T12:23:05+00:00';
+    const Date_RFC3339_EXT = '2020-07-06T12:23:05.000+00:00';
+    const Date_Cookie = 'Monday, 06-Jul-2020 12:23:05 GMT+0000';
+    const Date_RFC822 = 'Mon, 06 Jul 20 12:23:05 +0000';
+    const Date_RFC7231 = 'Mon, 06 Jul 2020 12:23:05 GMT';
     const Date_Int = 20200706122305;
     const Unix_Date = '1594038185';
 
@@ -34,16 +38,52 @@ class DateTimeTransformationTest extends TestCase
         $this->transformation = new DateTimeTransformation();
     }
 
-    public function testDateTimeTransformation()
+    public function testDateTimeISOTransformation()
     {
-        $original = new DateTime(self::Date_Origin);
-        $original = $original->format(self::ISO8601);
-        $expected = new \DateTimeImmutable(self::Date_Origin);
-        $expected = $expected->format(self::ISO8601);
+        $expected = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::ISO8601,self::Date_ISO);
+        $transformedValue = $this->transformation->transform(self::Date_ISO);
 
-        $transformedValue = $this->transformation->transform($original);
+        $this->assertEquals($expected->format(\DateTimeImmutable::ISO8601), $transformedValue);
+    }
 
-        $this->assertEquals($expected, $transformedValue);
+    public function testDateTimeAtomTransformation()
+    {
+        $expected = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::ATOM,self::Date_Atom);
+        $transformedValue = $this->transformation->transform(self::Date_Atom);
+
+        $this->assertEquals($expected->format(\DateTimeImmutable::ATOM), $transformedValue);
+    }
+
+    public function testDateTimeRFCExtTransformation()
+    {
+        $expected = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::RFC3339_EXTENDED,self::Date_RFC3339_EXT);
+        $transformedValue = $this->transformation->transform(self::Date_RFC3339_EXT);
+
+        $this->assertEquals($expected->format(\DateTimeImmutable::RFC3339_EXTENDED), $transformedValue);
+    }
+
+    public function testDateTimeCookieTransformation()
+    {
+        $expected = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::COOKIE,self::Date_Cookie);
+        $transformedValue = $this->transformation->transform(self::Date_Cookie);
+
+        $this->assertEquals($expected->format(\DateTimeImmutable::COOKIE), $transformedValue);
+    }
+
+    public function testDateTimeRFC822Transformation()
+    {
+        $expected = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::RFC822,self::Date_RFC822);
+        $transformedValue = $this->transformation->transform(self::Date_RFC822);
+
+        $this->assertEquals($expected->format(\DateTimeImmutable::RFC822), $transformedValue);
+    }
+
+    public function testDateTimeRFC7231Transformation()
+    {
+        $expected = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::RFC7231,self::Date_RFC7231);
+        $transformedValue = $this->transformation->transform(self::Date_RFC7231);
+
+        $this->assertEquals($expected->format(\DateTimeImmutable::RFC7231), $transformedValue);
     }
 
     public function testDateTimeToUnixTimestampTransformation()
@@ -51,7 +91,5 @@ class DateTimeTransformationTest extends TestCase
         $transformedValue = $this->transformation->transform(self::Date_Int);
 
         $this->assertEquals(self::Unix_Date, $transformedValue);
-
     }
-
 }
