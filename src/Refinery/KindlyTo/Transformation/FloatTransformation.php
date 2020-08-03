cae3c303bc9be97cdab75dd4148fc823ea14ac97
lukastocker <luka.stocker@concepts-and-training.de>
2020-08-03 17:34:05 +0200

    Refinery KindlyTo - Float: transform data into float and test transformation.

---
+++ src/Refinery/KindlyTo/Transformation/FloatTransformation.php
@@ -0,0 +1,64 @@
+<?php
+/* Copyright (c) 2020 Luka K. A. Stocker, Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Refinery\KindlyTo\Transformation;
+
+use ILIAS\Refinery\DeriveApplyToFromTransform;
+use ILIAS\Refinery\Transformation;
+use ILIAS\Refinery\ConstraintViolationException;
+
+class FloatTransformation implements Transformation {
+    const REG_STRING = '/^\s*(0|(-?[1-9]\d*([.,]\d+)?))\s*$/';
+    const REG_STRING_FLOATING = '/^\s*-?\d+[eE]-?\d+\s*$/';
+
+    use DeriveApplyToFromTransform;
+
+    /**
+     * @inheritdoc
+     */
+    public function transform($from) {
+        if(is_int($from)) {
+            return (float)$from;
+        }
+
+        if(is_bool($from)) {
+            return floatval($from);
+        }
+
+        if(is_string($from)) {
+
+            $preg_match_string = preg_match(self::REG_STRING, $from, $RegMatch);
+            $preg_match_floating_string = preg_match(self::REG_STRING_FLOATING, $from, $RegMatch);
+
+            if($preg_match_string) {
+                return floatval(str_replace(',','.', $from));
+            }
+
+            if($preg_match_floating_string) {
+                return floatval($from);
+            }
+
+            if (!$preg_match_string && !$preg_match_floating_string){
+                throw new ConstraintViolationException(
+                    sprintf('The value "%s" could not be transformed into an float',$from),
+                    'not_float',
+                    $from
+                );
+            }
+        }
+        if (!is_string($from)) {
+            throw new ConstraintViolationException(
+                sprintf('The value "%s" is no string and could not be transformed into an float',$from),
+                'not_float',
+                $from
+            );
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __invoke($from) {
+        return $this->transform($from);
+    }
+}
---
+++ tests/Refinery/KindlyTo/Transformation/FloatTransformationTest.php
@@ -0,0 +1,72 @@
+<?php
+/* Copyright (c) 2020 Luka K. A. Stocker, Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Tests\Refinery\KindlyTo\Transformation;
+
+require_once('./libs/composer/vendor/autoload.php');
+
+use ILIAS\Refinery\ConstraintViolationException;
+use ILIAS\Refinery\KindlyTo\Transformation\FloatTransformation;
+use ILIAS\Tests\Refinery\TestCase;
+
+/**
+ * Test transformations in this Group
+ */
+class FloatTransformationTest extends TestCase {
+
+    private $transformation;
+
+    public function setUp(): void {
+        $this->transformation = new FloatTransformation();
+    }
+
+    /**
+     * @dataProvider FloatTestDataProvider
+     * @param $originVal
+     * @param $expectedVal
+     */
+    public function testFloatTransformation($originVal, $expectedVal) {
+        $transformedValue = $this->transformation->transform($originVal);
+        $this->assertIsFloat($transformedValue);
+        $this->assertEquals($expectedVal, $transformedValue);
+    }
+
+    /**
+     * @dataProvider FailingTransformationDataProvider
+     * @param $failingVal
+     */
+    public function testFailingTransformations($failingVal) {
+        $this->expectNotToPerformAssertions();
+        try {
+            $transformedValue = $this->transformation->transform($failingVal);
+        } catch (ConstraintViolationException $exception) {
+            return;
+        }
+        $this->fail();
+    }
+
+    public function FailingTransformationDataProvider() {
+        return [
+            'null' => [null],
+            'empty' => [""],
+            'written_false' => ['false'],
+            'written_null' => ['null'],
+            'NaN' => [NAN],
+            'written_NaN' => ['NaN'],
+            'INF' => [INF],
+            'written_INF' => ['INF']
+        ];
+    }
+
+    public function FloatTestDataProvider() {
+        return [
+            'pos_bool' => [true, 1.0],
+            'neg_bool' => [false, 0.0],
+            'string_comma' => ['234,23', 234.23],
+            'string_floating_point' => ['7E10', 70000000000],
+            'int_val' => [23, 23.0],
+            'neg_int_val' => [-2, -2.0],
+            'zero_int' => [0, 0.0]
+        ];
+    }
+}
