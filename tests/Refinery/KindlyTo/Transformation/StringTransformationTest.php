9b834efe3972fa43fe3c064d2ada08d4751c556b
lukastocker <luka.stocker@concepts-and-training.de>
2020-08-03 17:33:40 +0200

    Refinery KindlyTo - String: transform data into string and test transformation.

---
+++ tests/Refinery/KindlyTo/Transformation/StringTransformationTest.php
@@ -0,0 +1,44 @@
+<?php
+/* Copyright (c) 2020 Luka K. A. Stocker, Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Tests\Refinery\KindlyTo\Transformation;
+
+require_once('./libs/composer/vendor/autoload.php');
+
+use ILIAS\Refinery\KindlyTo\Transformation\StringTransformation;
+use ILIAS\Tests\Refinery\TestCase;
+
+/**
+ * Test transformations in this Group
+ */
+class StringTransformationTest extends TestCase {
+
+    private $transformation;
+
+    public function setUp(): void {
+        $this->transformation = new StringTransformation();
+    }
+
+    /**
+     * @dataProvider StringTestDataProvider
+     * @param $originVal
+     * @param string $expectedVal
+     */
+    public function testStringTransformation($originVal, $expectedVal) {
+        $transformedValue = $this->transformation->transform($originVal);
+        $this->assertIsString($transformedValue);
+        $this->assertEquals($expectedVal, $transformedValue);
+    }
+
+    public function StringTestDataProvider() {
+        return [
+            'string_val' => ['hello', 'hello'],
+            'int_val' => [300, '300'],
+            'neg_int_val' => [-300, '-300'],
+            'zero_int_val' => [0, '0'],
+            'pos_bool' => [true, 'true'],
+            'neg_bool' => [false, 'false'],
+            'float_val' => [20.5, '20.5']
+        ];
+    }
+}
