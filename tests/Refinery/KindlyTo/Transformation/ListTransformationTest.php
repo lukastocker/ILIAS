b99eac94cd9de68742bf2672807197a1d9d7e587
lukastocker <luka.stocker@concepts-and-training.de>
2020-08-03 17:34:36 +0200

    Refinery KindlyTo - List: transform data into a list and test transformation.

---
+++ tests/Refinery/KindlyTo/Transformation/ListTransformationTest.php
@@ -0,0 +1,59 @@
+<?php
+/* Copyright (c) 2020 Luka K. A. Stocker, Extended GPL, see docs/LICENSE */
+
+namespace ILIAS\Tests\Refinery\KindlyTo\Transformation;
+
+require_once('./libs/composer/vendor/autoload.php');
+
+use ILIAS\Refinery\ConstraintViolationException;
+use ILIAS\Refinery\KindlyTo\Transformation\ListTransformation;
+use ILIAS\Refinery\To\Transformation\StringTransformation;
+use ILIAS\Tests\Refinery\TestCase;
+
+/**
+ * Test transformations in this Group
+ */
+class ListTransformationTest extends TestCase {
+    /**
+     * @dataProvider ArrayToListTransformationDataProvider
+     * @param $originValue
+     * @param $expectedValue
+     */
+    public function testListTransformation($originValue, $expectedValue) {
+        $transformList = new ListTransformation(new StringTransformation());
+        $transformedValue = $transformList->transform($originValue);
+        $this->assertIsArray($transformedValue);
+        $this->assertEquals($expectedValue, $transformedValue);
+    }
+
+    /**
+     * @dataProvider ArrayFailureDataProvider
+     * @param $origValue
+     */
+    public function testFailingTransformations($origValue) {
+        $this->expectNotToPerformAssertions();
+        $transformList = new ListTransformation(new StringTransformation());
+        try{
+            $result = $transformList->transform($origValue);
+        } catch (ConstraintViolationException $exception) {
+            return;
+        }
+        $this->fail();
+    }
+
+    public function ArrayToListTransformationDataProvider() {
+        return [
+            'first_arr' => [array('hello', 'world'), ['hello', 'world']],
+            'second_arr' => [array('hello2','world2'), ['hello2', 'world2']],
+            'string_val' => ['hello world',['hello world']]
+        ];
+    }
+
+    public function ArrayFailureDataProvider() {
+        return [
+            'empty_array' => [array()],
+            'null_array' => [array(null)],
+            'value_is_no_string' => [array('hello', 2)]
+        ];
+    }
+}
