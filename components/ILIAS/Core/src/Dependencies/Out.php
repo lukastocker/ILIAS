846a6e7266427668401b8c4aad4c87bf6c3005b5
Richard Klees <richard.klees@concepts-and-training.de>
2023-07-14 10:33:45 +0200

    Core/Dependencies: Read Definition of Component

---
+++ components/ILIAS/Core/src/Dependencies/Out.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\Core\Dependencies;
+
+/**
+ * A dependency where the component gives something to the world.
+ */
+class Out implements Dependency
+{
+    protected Name|string $name;
+    protected array $dependencies = [];
+
+    public function __construct(
+        protected OutType $type,
+        string $name,
+        protected ?string $class,
+        array $dependencies
+    ) {
+        if ($type !== OutType::INTERNAL) {
+            $name = new Name($name);
+            if (is_null($class)) {
+                throw new \LogicException(
+                    "Non-internal Out-Dependencies shall have a \$class."
+                );
+            }
+        }
+        $this->name = $name;
+        foreach ($dependencies as $d) {
+            $d->addDependant($this);
+        }
+    }
+
+    public function __toString(): string
+    {
+        return $this->type->value . ": " . $this->name;
+    }
+}
