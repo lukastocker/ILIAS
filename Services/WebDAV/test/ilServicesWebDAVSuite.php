12f4509f06b61116c5c4ad33695dff05b8925aa5
Fabian Schmid <fabian@sr.solutions>
2025-02-28 17:26:50 +0100

    [FIX] restored WebDAV Tests

--- Services/WebDAV/test/ilServicesWebDAVSuite.php
+++ Services/WebDAV/test/ilServicesWebDAVSuite.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -16,12 +18,12 @@
  *
  *********************************************************************/
 
-declare(strict_types=1);
-
 use PHPUnit\Framework\TestSuite;
 
+require_once __DIR__ . "/../../../libs/composer/vendor/autoload.php";
+
 /**
- * @author  Stephan Winiker <stephan.winiker@hslu.ch>
+ * @author Stephan Winiker <stephan.winiker@hslu.ch>
  * @version 1.0.0
  */
 class ilServicesWebDAVSuite extends TestSuite
@@ -29,9 +31,17 @@ class ilServicesWebDAVSuite extends TestSuite
     public static function suite(): ilServicesWebDAVSuite
     {
         $suite = new ilServicesWebDAVSuite();
+
+        require_once "./Services/WebDAV/test/traits/ilWebDAVCheckValidTitleTraitTest.php";
         $suite->addTestSuite("ilWebDAVCheckValidTitleTraitTest");
+
+        require_once "./Services/WebDAV/test/lock/ilWebDAVLockUriPathResolverTest.php";
         $suite->addTestSuite("ilWebDAVLockUriPathResolverTest");
+
+        require_once "./Services/WebDAV/test/dav/class.ilDAVContainerTest.php";
         $suite->addTestSuite("ilDAVContainerTest");
+
+        require_once "./Services/WebDAV/test/dav/class.ilDAVClientNodeTest.php";
         $suite->addTestSuite("ilDAVClientNodeTest");
 
         return $suite;
